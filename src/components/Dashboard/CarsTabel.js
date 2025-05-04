197985e6004d5d5102b8fccc15e7bd8d2c6558bb
itx-sherazi <sa0071429@gmail.com>
2025-05-04 11:12:30 -0700

    d

--- src/components/Dashboard/CarsTabel.js
+++ src/components/Dashboard/CarsTabel.js
@@ -170,7 +170,7 @@ export default function CarsTable() {
                 Year
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Speed
+              Transmission 
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Fuel
